e93dda7ceb68cc0f6a3e99e75e9d695fe063f737
mantriguna <mantriguna0106@gmail.com>
2024-09-24 20:01:42 +0530

    boilerplate

---
+++ src/main/java/com/project/RevshopP1/RevshopP1Application.java
@@ -0,0 +1,13 @@
+package com.project.RevshopP1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevshopP1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevshopP1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/RevshopP1/RevshopP1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.RevshopP1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevshopP1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
